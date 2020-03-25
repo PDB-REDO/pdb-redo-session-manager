e9025e82b03d825b255e22d433466aabbc88879e
maarten <maarten@f95637c9-dfad-4d73-ab43-77d88081e9bf>
2020-03-25 14:11:53 +0000

    bijna goed, dat inloggen enzo
    git-svn-id: svn+ssh://gitlab/srv/svn-repos/pdb-redo-session-manager/trunk@9 f95637c9-dfad-4d73-ab43-77d88081e9bf

--- webapp/index.js
+++ webapp/index.js
@@ -2,13 +2,6 @@ import '@babel/polyfill'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap';
 // import 'bootstrap/js/dist/modal'
-// import * as d3 from 'd3';
-// import $ from 'jquery';
-import { showLoginDialog } from './login';
 import './pdb-redo-bootstrap.scss';
 
-window.addEventListener("load", () => {
-	if (typeof username === 'undefined' || username == null)
-		showLoginDialog();
-});
 
