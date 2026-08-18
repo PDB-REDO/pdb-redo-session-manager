267895e8cc6f11a279f3cb530c90e4f9ac15055c
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-08-18 11:12:30 +0200

    Check early for valid PDB ID

--- src/prsmd.cpp
+++ src/prsmd.cpp
@@ -881,9 +881,7 @@ class DbController : public zeep::http::html_controller
 
 zeep::http::reply DbController::handle_get(const zeep::http::scope & /*scope*/, std::string pdbID)
 {
-	// const std::regex rx(R"((pdb_)?[0-9][0-9a-z]{3,7})", std::regex::icase);
-	// if (not std::regex_match(pdbID, rx))
-	// 	throw std::system_error(zeep::http::unprocessable_entity));
+	DataService::validatePDBID(pdbID);
 
 	zeep::to_lower(pdbID);
 	return zeep::http::reply::redirect(pdbID, zeep::http::status_type::see_other);
